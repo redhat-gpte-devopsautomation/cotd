4e7d0d196884da08425a18e3b43a2b871031ea3e
Stefano Picozzi <stefanopicozzi@dhcp-128-196.syd.redhat.com>
2016-09-14 16:43:45 +1000

    Cats. What else! Plus simplified edit changes to only rank.php file.

--- include/city.php
+++ include/city.php
@@ -19,58 +19,15 @@ foreach ($cities as $city) {
   }
 }
 
-$city = 'adelaide';
-$title[$city] = 'Adelaide';
-$triviatxt[$city] = '<p>Among most liveable cities in the world. An extraordinary achievement given so few live there. Green credentials backed by policy of frequent random power outages. <small>source:&nbsp;Martin Haese</small></p>';
-$rank = $_SESSION[$city];
-$next[$city] = $topcity;
-foreach ($cities as $nextcity) { if ($_SESSION[$nextcity] == ($rank+1)) { $next[$city] = $nextcity; } }
-$prev[$city] = $city;
-
-$city = 'melbourne';
-$title[$city] = 'Melbourne';
-$triviatxt[$city] = '<p>Famous for its dark narrow alley ways and cafe culture. When ordering do as the locals do and use the expression: Where is my goddam latte? The barista will love you for it. <small>source:&nbsp;Robert Doyle</small></p>';
-$rank = $_SESSION[$city];
-$next[$city] = $topcity;
-foreach ($cities as $nextcity) { if ($_SESSION[$nextcity] == ($rank+1)) { $next[$city] = $nextcity; } }
-$prev[$city] = $city;
-
-$city = 'sydney';
-$title[$city] = 'Sydney';
-$triviatxt[$city] = '<p>The international city. Features most expensive airport parking in the world. Dine before 8pm to beat the lockout laws. <small>source:&nbsp;Clover Moore</small></p>';
-$rank = $_SESSION[$city];
-$next[$city] = $topcity;
-foreach ($cities as $nextcity) { if ($_SESSION[$nextcity] == ($rank+1)) { $next[$city] = $nextcity; } }
-$prev[$city] = $city;
-
-$city = 'brisbane';
-$title[$city] = 'Brisbane';
-$triviatxt[$city] = '<p>Multiple modes of transport to get you straight back to the airport including train, taxi and tinny during the wet season. The dry season occurs on August 21 at approximately 2:pm. <small>source:&nbsp;Graham Quirk</small></p>';
-$rank = $_SESSION[$city];
-$next[$city] = $topcity;
-foreach ($cities as $nextcity) { if ($_SESSION[$nextcity] == ($rank+1)) { $next[$city] = $nextcity; } }
-$prev[$city] = $city;
-
-$city = 'perth';
-$title[$city] = 'Perth';
-$triviatxt[$city] = '<p>Perth. What is left to be said that has not been said. A lot actually as little has been said. <small>source:&nbsp;Lisa Scaffidi</small></p>';
-$rank = $_SESSION[$city];
-$next[$city] = $topcity;
-foreach ($cities as $nextcity) { if ($_SESSION[$nextcity] == ($rank+1)) { $next[$city] = $nextcity; } }
-$prev[$city] = $city;
-
-$city = 'hobart';
-$title[$city] = 'Hobart';
-$triviatxt[$city] = '<p>Famous for the record breaking MONA museum. MONA features in all 10 spots of the top 10 things to do. <small>source:&nbsp;Sue Hickey</small></p>';
-$rank = $_SESSION[$city];
-$next[$city] = $topcity;
-foreach ($cities as $nextcity) { if ($_SESSION[$nextcity] == ($rank+1)) { $next[$city] = $nextcity; } }
-$prev[$city] = $city;
+foreach ($cities as $city) {
+  $rank = $_SESSION[$city];
+  $next[$city] = $topcity;
+  foreach ($cities as $nextcity) { if ($_SESSION[$nextcity] == ($rank+1)) { $next[$city] = $nextcity; } }
+  $prev[$city] = $city;
+}
 
 foreach ($cities as $city) {
   $_SESSION['rank_'.$city] = $_SESSION[$city];
-  $_SESSION['title_'.$city] = $title[$city];
-  $_SESSION['triviatxt_'.$city] = $triviatxt[$city];
   $_SESSION['next_'.$city] = $next[$city];
   $_SESSION['prev_'.$city] = $prev[$city];
 }
--- include/rank.php
+++ include/rank.php
@@ -1,18 +1,34 @@
 <?php
 
-// Change rankings here
-// If adding new entries then also update 1) city.php 2) swipe-page.css 3) images
+// Change item rankings and assocviated data here
+// If adding new entries then also update swipe-page.css and images directory
 
 if (!isset($_SESSION)) { session_start(); }
 
+// Change ranking order here
 $_SESSION['adelaide'] = 0;
 $_SESSION['melbourne'] = 1;
 $_SESSION['sydney'] = 2;
 $_SESSION['brisbane'] = 3;
 $_SESSION['perth'] = 4;
 $_SESSION['hobart'] = 5;
-
 //$_SESSION['canberra'] = 6;
 //$_SESSION['darwin'] = 7;
+
+//Change page title here
+$_SESSION['title_adelaide'] = 'Ginger Cat';
+$_SESSION['title_melbourne'] = 'Prize Cat';
+$_SESSION['title_sydney'] = 'Birthday Cat';
+$_SESSION['title_brisbane'] = 'Toilet Cat';
+$_SESSION['title_perth'] = 'Fat Cat';
+$_SESSION['title_hobart'] = 'Wet Cat';
+
+// Change trivia here
+$_SESSION['trivia_adelaide'] = '<p>Among most liveable cities in the world. An extraordinary achievement given so few live there. Green credentials backed by policy of frequent random power outages. <small>Lord Mayor:&nbsp;Martin Haese</small></p>';
+$_SESSION['trivia_melbourne'] = '<p>Famous for its dark narrow alley ways and cafe culture. When ordering do as the locals do and use the expression: Where is my goddam latte? The barista will love you for it. <small>Lord Mayor:&nbsp;Robert Doyle</small></p>';
+$_SESSION['trivia_sydney'] = '<p>The international city. Features most expensive airport parking in the world. Dine before 8pm to beat the lockout laws. <small>Lord Mayor:&nbsp;Clover Moore</small></p>';
+$_SESSION['trivia_brisbane'] = '<p>Multiple modes of transport to get you quickly back to the airport including train, taxi and tinny during the wet season. The dry season occurs on August 21 at approximately 2:00 pm. <small>Lord Mayor:&nbsp;Graham Quirk</small></p>';
+$_SESSION['trivia_perth'] = '<p>Perth. What is left to be said that has not been said. A lot actually as little has been said. <small>Lord Mayor:&nbsp;Lisa Scaffidi</small></p>';
+$_SESSION['trivia_hobart'] = '<p>Famous for the record breaking MONA museum. MONA features in all 10 spots of the top 10 things to do. <small>Lord Mayor:&nbsp;Sue Hickey</small></p>';
 
 ?>
