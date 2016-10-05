249de843260ef48586843b9bde8d590f3626c721
StefanoPicozzi <StefanoPicozzi@gmail.com>
2016-10-05 14:50:53 +1100

    Cities selected

--- data/selector.php
+++ data/selector.php
@@ -1,8 +1,8 @@
 <?php
 
 // Select your list theme here
-$_SESSION['selector'] = 'cats';
-// $_SESSION['selector'] = 'cities';
+// $_SESSION['selector'] = 'cats';
+$_SESSION['selector'] = 'cities';
 include($_SESSION['selector'].'/rank.php');
 
 ?>
