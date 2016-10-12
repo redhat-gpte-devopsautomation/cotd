19a3199fe867713415e0692916d8a98132695a13
Stefano Picozzi <stefanopicozzi@Stefanos-MacBook-Pro.local>
2016-10-12 15:17:09 +1100

    x

--- data/selector.php
+++ data/selector.php
@@ -1,7 +1,11 @@
 <?php
 
 // Select your list theme here eg cats or cities
-$_SESSION['selector'] = 'cats';
+if ( $selector = getenv('SELECTOR') ) {
+    $_SESSION['selector'] = $selector;
+} else {
+    $_SESSION['selector'] = 'cats';
+}
 include($_SESSION['selector'].'/rank.php');
 
 ?>
