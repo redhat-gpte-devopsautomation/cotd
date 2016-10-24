fc1c711013892222e5996ff9627bd16eae1de520
Gogs <gogs@fake.local>
2016-10-24 15:57:01 +1100

    x

--- data/selector.php
+++ data/selector.php
@@ -1,11 +1,18 @@
 <?php
 
-// Select your list theme here eg cats or cities
-if ( $selector = getenv('SELECTOR') ) {
-    $_SESSION['selector'] = $selector;
-} else {
-    $_SESSION['selector'] = 'cats';
-}
+
+$ini_file = '/etc/config/cotd.properties';                                                                                                               
+                                                                                                                                                            
+// Select your list theme here eg cats or cities                                                                                                            
+if ( $selector = getenv('SELECTOR') ) {                                                                                                                     
+    $_SESSION['selector'] = $selector;                                                                                                                      
+} elseif ( file_exists($ini_file) ) {                                                                                                                       
+    $ini_array = parse_ini_file($ini_file);                                                                                                                 
+    $_SESSION['selector'] = $ini_array['selector'];                                                                                                         
+} else {                                                                                                                                                    
+    $_SESSION['selector'] = 'cats';                                                                                                                         
+}                                        
+
 include($_SESSION['selector'].'/rank.php');
 
 ?>
