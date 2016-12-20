01ded9fa3702d4980265f74eccc32a94fb151278
Gogs <gogs@fake.local>
2016-12-20 17:11:55 +1100

    x

--- data/db.php
+++ data/db.php
@@ -22,6 +22,77 @@ try {
     $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $stmt = $dbh -> prepare("
+        SELECT i.id AS :id, i.name AS :name, SUM(r.rating) AS :total 
+        FROM ratings r, items i
+        WHERE r.name = i.name
+        GROUP BY name
+        UNION
+        SELECT id AS :id, name AS :name, 0 AS :total
+        FROM items
+        WHERE name NOT IN
+        (SELECT DISTINCT name
+        FROM ratings)
+        ORDER BY total DESC, id ASC;
+    ");
+
+    $id = 0;
+    $name = '';
+    $total = 0;
+    $theme = $_SESSION['selector'];
+
+    $stmt -> bindParam(':theme', $theme, PDO::PARAM_STR);
+    $stmt -> bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt -> bindParam(':total', $total, PDO::PARAM_STR);
+    $stmt -> bindParam(':id', $id, PDO::PARAM_STR);
+
+    $names[] = array();
+    $i = 0;
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+        $names[$i] = array('name' => $row[0], 'rank' => $i);
+        $i = $i + 1;
+    }
+  
+} catch(Exception $e) {
+   $_SESSION['message'] = "Failed to get rank of items. ".$e;
+   header("Location: error.php");
+}
+
+try {
+    $dbh = new PDO("mysql:host=$mysql_hostname;port=$mysql_port;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $dbh -> prepare("
+        UPDATE items
+        SET rank = :rank
+        WHERE theme = :theme
+         AND  name = :name;
+    ");
+
+    $name = '';
+    $total = 0;
+    $theme = $_SESSION['selector'];
+    $count = sizeof( $names );
+    
+    for ($i=0; $i < $count; $i++) {
+        $rank = $names[$i]['rank'];
+        $name = $names[$i]['name'];
+        $stmt -> bindParam(':rank', $rank, PDO::PARAM_STR);
+        $stmt -> bindParam(':theme', $theme, PDO::PARAM_STR);
+        $stmt -> bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+  
+} catch(Exception $e) {
+   $_SESSION['message'] = "Failed to update rank of items. ".$e;
+   header("Location: error.php");
+}
+
+try {
+    $dbh = new PDO("mysql:host=$mysql_hostname;port=$mysql_port;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $dbh -> prepare("
         SELECT
             name AS :name,
             rank AS :rank,
