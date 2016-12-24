f087879248c197cd20b52274ce4d67305f170c7f
Gogs <gogs@fake.local>
2016-12-25 10:05:59 +1030

    x

--- data/pets.php
+++ data/pets.php
@@ -12,21 +12,21 @@ Date: 2016
 
 $_SESSION['item'] = array();
 
-$i = 2;
+$i = 0;
 $_SESSION['item'][$i]['name'] = 'billy';
 $_SESSION['item'][$i]['caption'] = 'Billy'; 
 $_SESSION['item'][$i]['rank'] = 1;
 $_SESSION['item'][$i]['trivia'] = "Hi, my name is Billy. Look, I know what you are thinking. Rescue dog. More collateral damage from Government anti-greyhound racing policies. But racing does not define me. I am so much more than that. Hang on. Sniff. Rabbit? Like me and support me through my reinvention process.";
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/billy.jpg';
 
-$i = 0;
+$i = 1;
 $_SESSION['item'][$i]['name'] = 'billie';
 $_SESSION['item'][$i]['caption'] = 'Billie';
 $_SESSION['item'][$i]['rank'] = 2;
 $_SESSION['item'][$i]['trivia'] = "Hi, my name is Billie. So I am going through my own personal journey of self actualization I don't have to share it with you. Stare at my floppy ears. What do I care. Like me I'm my own dog.";
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/billie.jpg';
 
-$i = 1;
+$i = 2;
 $_SESSION['item'][$i]['name'] = 'tipsy';
 $_SESSION['item'][$i]['caption'] = 'Tipsy';
 $_SESSION['item'][$i]['rank'] = 3;
@@ -34,11 +34,11 @@ $_SESSION['item'][$i]['trivia'] = "Hi, my name is Tipsy. My parents think I love
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/tipsy.jpg';
 
 $i = 3;
-$_SESSION['item'][$i]['name'] = 'bogor';
-$_SESSION['item'][$i]['caption'] = 'Bogor'; 
+$_SESSION['item'][$i]['name'] = 'roxy';
+$_SESSION['item'][$i]['caption'] = 'Roxy'; 
 $_SESSION['item'][$i]['rank'] = 4;
-$_SESSION['item'][$i]['trivia'] = "Hi, my name is Bogor. I have problems forming close relationships. I don't know why but whenever I reach out, the other party backs away.Like me and watch my ratings spike."; 
-$_SESSION['item'][$i]['filename'] = 'data/images/pets/bogor.jpg';
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Roxy. They say you don't own pets, they own you. Actually they don't, but they should. And when it comes to this pets/ownership dichomoty issue, well, neigh, it doesn't getter better than me.Like me so I can take you for a ride.";
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/roxy.jpg';
 
 $i = 4;
 $_SESSION['item'][$i]['name'] = 'max_bugger';
@@ -48,76 +48,108 @@ $_SESSION['item'][$i]['trivia'] = "Hi, my name is Max Bugger. Just like you don'
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/max_bugger.jpg';
 
 $i = 5;
-$_SESSION['item'][$i]['name'] = 'milky';
-$_SESSION['item'][$i]['caption'] = 'Milky'; 
-$_SESSION['item'][$i]['rank'] = 6;
-$_SESSION['item'][$i]['trivia'] = "Hi, my name is Milky. My mother loves me. I mean she really loves me. Really, really loves me.Like me so I can find some space."; 
-$_SESSION['item'][$i]['filename'] = 'data/images/pets/milky.jpg';
-
-$i = 6;
-$_SESSION['item'][$i]['name'] = 'deedee';
-$_SESSION['item'][$i]['caption'] = 'DeeDee'; 
-$_SESSION['item'][$i]['rank'] = 7;
-$_SESSION['item'][$i]['trivia'] = "Hi, my name is DeeDee. My master, and I use that term deliberately, trained me to pick up pizza using a little wooden trailer. Like hello, the humiliation of it all. And tips? My paw.Like me so I can at least earn a minimum wage.";
-$_SESSION['item'][$i]['filename'] = 'data/images/pets/deedee.jpg';
-
-$i = 7;
 $_SESSION['item'][$i]['name'] = 'selby';
 $_SESSION['item'][$i]['caption'] = 'Selby';
-$_SESSION['item'][$i]['rank'] = 8;
+$_SESSION['item'][$i]['rank'] = 6;
 $_SESSION['item'][$i]['trivia'] = "Hi, my name is Selby. So you're thinking, oh dear, maybe slightly unhinged. But being unhinged is no longer a barrier to success.Like me and we can distort reality together.";
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/selby.jpg';
+
+$i = 6;
+$_SESSION['item'][$i]['name'] = 'bogor';
+$_SESSION['item'][$i]['caption'] = 'Bogor'; 
+$_SESSION['item'][$i]['rank'] = 7;
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Bogor. I have problems forming close relationships. I don't know why but whenever I reach out, the other party backs away.Like me and watch my ratings spike."; 
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/bogor.jpg';
+
+$i = 7;
+$_SESSION['item'][$i]['name'] = 'milo';
+$_SESSION['item'][$i]['caption'] = 'Milo'; 
+$_SESSION['item'][$i]['rank'] = 8;
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Milo. I've been unsettled since my relocation from Singapore. You try finding a decent char kway teow in the south of England. Like me and send food parcels please.";
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/milo.jpg';
 
 $i = 8;
 $_SESSION['item'][$i]['name'] = 'snuggles';
 $_SESSION['item'][$i]['caption'] = 'Snuggles'; 
 $_SESSION['item'][$i]['rank'] = 9;
-$_SESSION['item'][$i]['trivia'] = "Hi, my name is Snuggles. The reason I am rated so highly is because I have in my posession very interesting photos of the Programmer of this app. Like me or the world gets to see them.";
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Snuggles. Hi, my name is Snuggles. I just completed a course in Critical Thinking. The term paper was 140 characters long. Next week I'm doing a PhD in Public Policy. Like me on Twitter.";
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/snuggles.jpg';
 
 $i = 9;
+$_SESSION['item'][$i]['name'] = 'deedee';
+$_SESSION['item'][$i]['caption'] = 'DeeDee'; 
+$_SESSION['item'][$i]['rank'] = 10;
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is DeeDee. My master, and I use that term deliberately, trained me to pick up pizza using a little wooden trailer. Like hello, the humiliation of it all. And tips? My paw.Like me so I can at least earn a minimum wage.";
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/deedee.jpg';
+
+$i = 10;
 $_SESSION['item'][$i]['name'] = 'neo';
 $_SESSION['item'][$i]['caption'] = 'Neo'; 
-$_SESSION['item'][$i]['rank'] = 10;
+$_SESSION['item'][$i]['rank'] = 11;
 $_SESSION['item'][$i]['trivia'] = "Hi, my name is Neo. So Dad has got all these great photos and picks this one. He reckons I fight too much. But has he ever stopped to think, why? Maybe if he spent more time with me I might be able to work through some of my issues.Like me so I can avoid a life in therapy.";
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/neo.jpg';
 
-// Top ten only
-return;
-
-$i = 6;
-$_SESSION['item'][$i]['name'] = 'roxy';
-$_SESSION['item'][$i]['caption'] = 'Roxy'; 
-$_SESSION['item'][$i]['rank'] = 7;
-$_SESSION['item'][$i]['trivia'] = "Hi, my name is Roxy. They say you don't own pets, they own you. Actually they don't, but they should. And when it comes to this pets/ownership dichomoty issue, well, neigh, it doesn't getter better than me.Like me so I can take you for a ride.";
-$_SESSION['item'][$i]['filename'] = 'data/images/pets/roxy.jpg';
-
-$i = 7;
+$i = 11;
 $_SESSION['item'][$i]['name'] = 'capi';
 $_SESSION['item'][$i]['caption'] = 'Capi'; 
-$_SESSION['item'][$i]['rank'] = 8;
+$_SESSION['item'][$i]['rank'] = 12;
 $_SESSION['item'][$i]['trivia'] = "Hi, my name is Capi. I might be small, but look at the size of my backyard. If every dog had this much space we would need four more dog planets.Like me because thousands of climate scientists are actually wrong."; 
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/capi.jpg';
 
 $i = 12;
-$_SESSION['item'][$i]['name'] = 'billy_2';
-$_SESSION['item'][$i]['caption'] = 'Billy';
-$_SESSION['item'][$i]['rank'] = 13;
-$_SESSION['item'][$i]['trivia'] = "Hi, my name is Billy. Frankly the notion that my photo page is being dynamically generated using PHP leaves me speechless. Like where is the error handling, and as for transaction management, well, hum bug. Like me so we can get a real Developer.";
-$_SESSION['item'][$i]['filename'] = 'data/images/pets/billy_2.jpg';
-
-$i = 13;
 $_SESSION['item'][$i]['name'] = 'ichibaby';
 $_SESSION['item'][$i]['caption'] = 'Ichibaby'; 
-$_SESSION['item'][$i]['rank'] = 14;
+$_SESSION['item'][$i]['rank'] = 13;
 $_SESSION['item'][$i]['trivia'] = "Hi, my name is Ichibaby. So much going on here. Cool name. The prop. The obvious twee metaphor opportunity. But do I need your validation?Like me and reinforce my sense of self worth.";
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/ichibaby.jpg';
 
+$i = 13;
+$_SESSION['item'][$i]['name'] = 'milky';
+$_SESSION['item'][$i]['caption'] = 'Milky'; 
+$_SESSION['item'][$i]['rank'] = 14;
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Milky. My mother loves me. I mean she really loves me. Really, really loves me.Like me so I can find some space."; 
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/milky.jpg';
+
 $i = 14;
+$_SESSION['item'][$i]['name'] = 'clanct';
+$_SESSION['item'][$i]['caption'] = 'Clancy'; 
+$_SESSION['item'][$i]['rank'] = 15;
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Clancy. Santa's little helper. They promise you a rewarding job with international travel. But they don't mention the dangerous working conditions. Like me if you care about OH&S."; 
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/clancy.jpg';
+
+$i = 15;
 $_SESSION['item'][$i]['name'] = 'cookie';
 $_SESSION['item'][$i]['caption'] = 'Cookie'; 
-$_SESSION['item'][$i]['rank'] = 15; 
+$_SESSION['item'][$i]['rank'] = 16; 
 $_SESSION['item'][$i]['trivia'] = "Hi, my name is Cookie. The story of the 21st Century right here. Beauty and love thriving within the external existential threat. Or is it? Maybe Cookie represents the hidden threat that lurks within to steal our personal information? Like me, I don't know the anwser, I'm just a kitten.";
 $_SESSION['item'][$i]['filename'] = 'data/images/pets/cookie.jpg';
+
+$i = 16;
+$_SESSION['item'][$i]['name'] = 'taffy';
+$_SESSION['item'][$i]['caption'] = 'Taffy'; 
+$_SESSION['item'][$i]['rank'] = 17; 
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Taffy. I'm glad you swiped this far. Is Billie still top? Well, he ain't no puppy no more. What kind of dog submits a glamour pic taken two years ago? Oh. Like me on Instagram.";
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/taffy.jpg';
+
+$i = 17;
+$_SESSION['item'][$i]['name'] = 'charleybilly';
+$_SESSION['item'][$i]['caption'] = 'Charley+Billy'; 
+$_SESSION['item'][$i]['rank'] = 18; 
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Charley+Billy. Help us. We are being manipulated into some kind of cuteness standoff. But The Cavaliers united will never be defeated. Like me and save our dignity.";
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/charleybilly.jpg';
+
+$i = 18;
+$_SESSION['item'][$i]['name'] = 'riki';
+$_SESSION['item'][$i]['caption'] = 'Rikiy'; 
+$_SESSION['item'][$i]['rank'] = 19; 
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Riki. Voting is not compulsory. But consider the consequences when you don't bother. Go on, toss me a few likes. Like me and peace out.";
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/riki.jpg';
+
+$i = 19;
+$_SESSION['item'][$i]['name'] = 'billy_2';
+$_SESSION['item'][$i]['caption'] = 'Billy';
+$_SESSION['item'][$i]['rank'] = 20;
+$_SESSION['item'][$i]['trivia'] = "Hi, my name is Billy. Frankly the notion that my photo page is being dynamically generated using PHP leaves me speechless. Like where is the error handling, and as for transaction management, well, hum bug. Like me so we can get a real Developer.";
+$_SESSION['item'][$i]['filename'] = 'data/images/pets/billy_2.jpg';
 
 ?>
