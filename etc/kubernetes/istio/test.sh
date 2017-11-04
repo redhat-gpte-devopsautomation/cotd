63bcb717b0b47881b1000cd99a54a6d8c712e809
Stefano Picozzi <StefanoPicozzi@gail.com>
2017-11-05 09:30:01 +1100

    x

--- etc/kubernetes/istio/test.sh
+++ etc/kubernetes/istio/test.sh
@@ -1,7 +1,12 @@
-export COTD_URL=$(kubectl get pod -n istio-system -l istio=ingress -o 'jsonpath={.items[0].status.hostIP}'):$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+# Restart istio-system
+# kubectl delete pods --all -n=istio-system
+# sleep 5
 
+# Extract URL
+export COTD_URL=$(kubectl get pod -n istio-system -l istio=ingress -o 'jsonpath={.items[0].status.hostIP}'):$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
 echo $COTD_URL
 
+# Test endpoints
 curl -s http://$COTD_URL/cats/item.php | grep "data/images" | awk '{print $5}'; 
 curl -s http://$COTD_URL/cats/item.php | grep "data/images" | awk '{print $5}'; 
 curl -s http://$COTD_URL/cats/item.php | grep "data/images" | awk '{print $5}'; 
