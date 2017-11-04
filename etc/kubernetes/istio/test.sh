2095d2476b451301437c803787c0c45d9ed0cb67
Stefano Picozzi <StefanoPicozzi@gail.com>
2017-11-05 09:46:52 +1100

    x

--- etc/kubernetes/istio/test.sh
+++ etc/kubernetes/istio/test.sh
@@ -1,6 +1,7 @@
 # Restart istio-system
-# kubectl delete pods --all -n=istio-system
-# sleep 5
+kubectl delete pods --all -n=istio-system
+sleep 5
+kubectl get pods -n=istio-system
 
 # Extract URL
 export COTD_URL=$(kubectl get pod -n istio-system -l istio=ingress -o 'jsonpath={.items[0].status.hostIP}'):$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
